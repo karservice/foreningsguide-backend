8bb74b1db683e2a1af302f3aa29fd514a230d25b
Lucas Sevelin <lucas.sevelin@gmail.com>
2024-08-20 17:43:42 +0200

    Added persistent local storage

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,14 @@
-export default () => ({});
+// config/plugins.ts
+import { Strapi } from '@strapi/strapi';
+
+export default ({ env }: { env: Strapi['config']['get'] }) => ({
+  upload: {
+    config: {
+      provider: 'local',
+      providerOptions: {
+        sizeLimit: 5000000, // Set file size limit in bytes
+        path: 'public/uploads',
+      },
+    },
+  },
+});
