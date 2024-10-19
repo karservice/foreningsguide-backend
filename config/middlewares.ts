46922af004f84d79c200246bd0107b2552f3a187
Lucas Sevelin <lucas.sevelin@gmail.com>
2024-10-19 16:07:51 +0200

    Added options to middleware

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -12,7 +12,7 @@ module.exports = [
     name: 'strapi::cors',
     config: {
       origin: ['http://localhost:3000', 'https://foreningsguide-frontend.vercel.app/', 'https://foreningsguide.studentlivet.se'],
-      methods: ['GET'],
+      methods: ['GET', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Access-Control-Allow-Headers', 'Access-Control-Allow-Origin'],
     },
   },
